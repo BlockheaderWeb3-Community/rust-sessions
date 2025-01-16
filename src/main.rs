e0145ee1315661122ab3693d7894efc0782793b8
JayWebtech <jethroadamzy@gmail.com>
2025-01-16 13:13:03 +0100

    jethro's commit

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
+
 fn main() {
-    intro_to_u();
+    intro_to_u()
 }
 
-
-// function to encapsulate all integers
 fn intro_to_u() {
     let sum_result: u8 = sum(5, 10);
-    println!("the sum result is: {}", sum_result);   
+    let subtraction_result: u8 = subtraction(10, 5);
+    let multiplication_result: u8 = multiplication(5, 10);
+    let division_result: u8 = division(10, 5);
 
-
+    println!("The sum is: {}", sum_result);
+    println!("The difference is: {}", subtraction_result);
+    println!("The multiplication is: {}", multiplication_result);
+    println!("The division is: {}", division_result);
 }
 
-fn sum(x: u8, y: u8) -> u8 {
-    x + y // implicit return
-//    return x + y; // explicit return
+fn sum(x:u8, y:u8) -> u8 {
+    x + y
 }
 
+fn subtraction(x:u8, y:u8) -> u8 {
+    x - y
+}
 
-// subtract
-// multiplication
-// division
+fn multiplication(x:u8, y:u8) -> u8 {
+    x * y
+}
 
-
+fn division(x:u8, y:u8) -> u8 {
+    x / y
+}
