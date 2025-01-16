b809d6ba3c7a9721a783f1c1617097f3c09fbe1c
seun <seunemokhare@gmail.com>
2025-01-16 13:15:56 +0100

    rust assigment

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ fn main() {
 // function to encapsulate all integers
 fn intro_to_u() {
     let sum_result: u8 = sum(5, 10);
-    println!("the sum result is: {}", sum_result);   
-
+    println!("the sum result is: {}", sum_result);
+    let sub_result: u16 = sub(8, 8);
+    println!("the sub result is: {}, sub_result");
+    let mul_result: u16 = mul(2, 4);
+    println!("the mul result is: {}", mul_result);
+    let divide_result: u16 = divide(10, 10);
+    println!("the divide result is: {}", divide_result);   
 
 }
 
@@ -16,9 +21,17 @@ fn sum(x: u8, y: u8) -> u8 {
 //    return x + y; // explicit return
 }
 
-
 // subtract
+fn sub(x: u16, y: u16) -> u16 {
+    x - y
+}
 // multiplication
-// division
+fn mul(x: u16, y: u16) -> u16 {
+    x * y
+}
 
+// division
+fn divide(x: u16, y: u16) -> u16 {
+    x / y
+}
 
