0d636f55bfe3df4555165d8551ee795ec3b21c3c
Soloxie <solomonu928@gmail.com>
2025-01-16 13:22:56 +0100

    operational functions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
+
 fn main() {
+   
     intro_to_u();
 }
 
-
-// function to encapsulate all integers
-fn intro_to_u() {
-    let sum_result: u8 = sum(5, 10);
-    println!("the sum result is: {}", sum_result);   
-
-
+fn intro_to_u(){
+let sum_result : u8 = sum(5, 5);
+let sub_result : u8 = subtract(10, 5);
+let multiply_result : u8 = multiply(10, 5);
+let divide_result : u8 = divide(10, 5);
+println!("the sum result is: {}", sum_result);
+println!("the subtraction result is: {}", sub_result);
+println!("the multiplication result is: {}", multiply_result);
+println!("the dvision result is: {}", divide_result);
 }
 
-fn sum(x: u8, y: u8) -> u8 {
-    x + y // implicit return
-//    return x + y; // explicit return
+fn sum(x: u8, y:u8) -> u8 {
+x + y
+// return x+y;//explecit return
 }
 
+fn subtract(x: u8, y:u8) -> u8 {
+x-y
+}
 
-// subtract
-// multiplication
-// division
+fn multiply(x: u8, y:u8) -> u8 {
+x*y
+}
 
-
+fn divide(x: u8, y:u8) -> u8 {
+x/y
+}
