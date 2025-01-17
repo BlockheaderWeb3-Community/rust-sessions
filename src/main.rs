31142893c2387e7cd5a27fe4eaab4f85b5c64276
seun <seunemokhare@gmail.com>
2025-01-17 10:15:09 +0100

    iseven & sumof even num

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     intro_to_u();
 }
@@ -5,18 +6,22 @@ fn main() {
 
 // function to encapsulate all integers
 fn intro_to_u() {
-    let sum_result: u8 = sum(5, 10);
+    let sum_result: u16 = sum(5, 10);
     println!("the sum result is: {}", sum_result);
     let sub_result: u16 = sub(8, 8);
     println!("the sub result is: {}", sub_result);
     let mul_result: u16 = mul(2, 4);
     println!("the mul result is: {}", mul_result);
     let divide_result: u16 = divide(10, 10);
-    println!("the divide result is: {}", divide_result);   
+    println!("the divide result is: {}", divide_result);
+    let sum_even_result: u16 = sum_even_num(10, 5);
+    println!("the sum to evem result is {}", sum_even_result ); 
+    let is_even_result: bool = is_even_num(10, 5);
+    println!("the is even num is {}", is_even_result ); 
 
 }
 
-fn sum(x: u8, y: u8) -> u8 {
+fn sum(x: u16, y: u16) -> u16 {
     x + y // implicit return
 //    return x + y; // explicit return
 }
@@ -33,5 +38,18 @@ fn mul(x: u16, y: u16) -> u16 {
 // division
 fn divide(x: u16, y: u16) -> u16 {
     x / y
+}
+
+fn sum_even_num(x: u16,y: u16) -> u16 {
+    x + y
+}
+
+fn is_even_num(x: u16, y: u16) -> bool {
+    let z: u16 = sum_even_num(x, y);
+        if (z) % 2 ==0 {
+        true
+    } else {
+        false
+    }
 }
 
