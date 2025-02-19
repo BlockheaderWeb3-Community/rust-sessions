35c3cf7072b23f681a71b433aae0dae43e4cc20c
seun <seunemokhare@gmail.com>
2025-02-19 15:38:11 +0100

    implement constructor

---
+++ src/constructor.rs
@@ -0,0 +1,29 @@
+pub struct Book<'a> {
+    pub title: &'a str,
+    pub author: &'a str, 
+    pub year: i32,
+    pub likes: i32,
+
+
+}
+
+impl<'a> Book<'a> {
+pub fn new(title: &'a str, author: &'a str, year: i32) -> Book<'a> {
+   Book {
+        title,
+        author,
+        year,
+        likes: 0
+    }
+
+}
+
+}
+
+pub fn constructors () {
+let new_book = Book::new (
+    "The Hidden Ember Ignited the Sky",
+    "Jemiiah",
+    2027,
+);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-//mod strings;
-mod float;
-mod signed;
-mod string;
-mod unsigned;
-mod user_struct;
+// //mod strings;
+// mod float;
+// mod signed;
+// mod string;
+// mod unsigned;
+// mod user_struct;
+mod constructor;
 
-fn main() {
-    unsigned::intro_to_u();
-    signed::intro_to_i();
-    float::intro_to_float();
-    string::strings();
-    user_struct::user_registry();
+ fn main() {
+//     unsigned::intro_to_u();
+//     signed::intro_to_i();
+//     float::intro_to_float();
+//     string::strings();
+//     user_struct::user_registry();
+        constructor::constructors();
 }
