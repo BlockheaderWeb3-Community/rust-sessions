e732ec38f0335cf790a4ba076e8b8feb702b24b7
GideonBature <infoaboutgideon@gmail.com>
2025-03-28 12:23:06 +0100

    Feat: complete outro

--- traits/14_outro.rs
+++ traits/14_outro.rs
@@ -1,3 +1,7 @@
+use std::cmp::Ordering;
+use std::convert::From;
+use std::fmt::Debug;
+use std::ops::Add;
 // TODO: Define a new `SaturatingU16` type.
 //   It should hold a `u16` value.
 //   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
@@ -8,7 +12,56 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+#[derive(PartialEq, Debug)]
+pub struct SaturatingU16 {
+    pub value: u16,
+}
 
-struct SaturatingU16 {
-    value: u16,
+impl From<SaturatingU16> for u16 {
+    fn from(value: SaturatingU16) -> Self {
+        value.value
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16 {
+            value: value as u16,
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16 { value: *value }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16 {
+            value: *value as u16,
+        }
+    }
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+    fn add(self, rhs: SaturatingU16) -> Self::Output {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+
+impl PartialOrd for SaturatingU16 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.value.cmp(&other.value))
+    }
 }
