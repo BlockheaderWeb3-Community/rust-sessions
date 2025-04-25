382246f1ac12061da3fd78593e700edc12f70a96
martin machiebe <martinmachiebe21@gmail.com>
2025-04-25 18:42:04 +0100

    updates

--- src/day_01/enums.rs
+++ src/day_01/enums.rs
@@ -40,17 +40,25 @@ pub fn enums_details() {
         _ => println!("numbe:r 10 not found"),
     }
 
-    let payment = PaymentMethod::Crypto { coin: "S".to_string(), address: "0x124".to_string() };
+    let payment = PaymentMethod::Crypto {
+        coin: "S".to_string(),
+        address: "0x124".to_string(),
+    };
 
-    if let PaymentMethod::Cash = payment  {
+    if let PaymentMethod::Cash = payment {
         println!("Payment using if let")
-    };  
+    };
 
     match payment {
         PaymentMethod::Cash => println!("This is {:#?}", PaymentMethod::Cash),
-        PaymentMethod::Transfer(val) if val == "Successful".to_string() => println!("This is {:#?}", PaymentMethod::Transfer("Successful".to_string())),
-        PaymentMethod::Crypto {coin: val, address: val2} if val == "S" && val2 == "0x124" => println!("Pay with crypto"),
-        _ => println!("No payment")
+        PaymentMethod::Transfer(val) if val == "Successful".to_string() => println!(
+            "This is {:#?}",
+            PaymentMethod::Transfer("Successful".to_string())
+        ),
+        PaymentMethod::Crypto {
+            coin: val,
+            address: val2,
+        } if val == "S" && val2 == "0x124" => println!("Pay with crypto"),
+        _ => println!("No payment"),
     }
-
 }
